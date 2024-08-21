fd10f4a054b78e2d64b7d342e83693bfa6eb0354
DanielWorker <danila.work.today@gmail.com>
2024-08-21 21:36:09 +0200

    split deposit calculation into two files

--- app/api/deposit_api.py
+++ app/api/deposit_api.py
@@ -5,25 +5,15 @@ from dateutil.relativedelta import relativedelta
 from fastapi import APIRouter, HTTPException
 from pydantic import ValidationError
 
+from .calculate_deposit import calculate_deposit
 from ..schemas.DepositsSchema import DepositSchema
 
 DepositApi = APIRouter(prefix='/deposits')
 
 
 @DepositApi.post('/calculate')
-def calculate_deposit(deposit: DepositSchema) -> Dict[str, float]:
+def calculate_deposit_api(deposit: DepositSchema) -> Dict[str, float]:
     try:
-        amount = deposit.amount
-        start_date = datetime.strptime(deposit.date, '%d.%m.%Y')
-        monthly_rate = deposit.rate / 12 / 100
-        results = {}
-
-        for i in range(deposit.periods):
-            amount += amount * monthly_rate
-            current_date = start_date + relativedelta(months=i)
-            results[current_date.strftime('%d.%m.%Y')] = round(amount, 2)
-
-        return results
-
+        return calculate_deposit(deposit)
     except ValidationError as e:
         raise HTTPException(status_code=400, detail=str(e))
